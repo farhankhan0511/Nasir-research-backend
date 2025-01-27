387eec026e3d8acaa51728978ee8e0aa331723d0
farhankhan0511 <farhankhan051104@gmail.com>
2025-01-28 00:36:10 +0530

    m

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors({
 }))
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({limit:"16kb",extended:true}));
-app.use(express.static("public"));
+
 
 import recrouter from "./router.js"
 app.use("/rec",recrouter)
